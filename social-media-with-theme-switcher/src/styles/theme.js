6c7c42d13386f7c6ac45a360ecc8432b715e89d9
AKHIL POKLE <akhilpokle96@gmail.com>
2020-05-29 19:57:39 +0530

    coding bootcamp initial

---
+++ social-media-with-theme-switcher/src/styles/theme.js
@@ -0,0 +1,16 @@
+export const lightTheme =  {
+
+    body: 'hsl(0, 0%, 100%)',
+    topbody: 'hsl(225, 100%, 98%)',
+    Card: 'hsl(227, 47%, 96%)',
+    p: 'hsl(228, 12%, 44%)',
+    h1: 'hsl(230, 17%, 14%)'
+}
+
+export const darkTheme = {
+    body: 'hsl(230, 17%, 14%)',
+    topbody: 'hsl(232, 19%, 15%)',
+    card: 'hsl(228, 28%, 20%)',
+    p: 'hsl(228, 34%, 66%)',
+    h1: 'hsl(0, 0%, 100%)'
+}
