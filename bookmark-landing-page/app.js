c7df9bd9cf87337fb0533549bc14781b05b19d61
AKHIL POKLE <akhilpokle96@gmail.com>
2020-06-03 22:34:07 +0530

    even more css

--- bookmark-landing-page/app.js
+++ bookmark-landing-page/app.js
@@ -1,8 +1,10 @@
-console.log("heelo");
 
 const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
+
+const faqAnswer = document.querySelectorAll('.faq-answer');
+
 
 let ele = document.querySelectorAll('.section-3 > div');
 
@@ -44,5 +46,34 @@ window.onload = () => {
     ele[1].style.display = 'none';
     ele[2].style.display = 'none';
     one.setAttribute('id','red-border');
+    
+    for(let i=0;i<faqAnswer.length;i++){
+        faqAnswer[i].style.display = 'none';
+        console.log(faqAnswer[i]);
+    }
 }
 
+
+
+console.log(faqAnswer);
+
+const faqQue = document.querySelectorAll('.faq-question');
+console.log(faqQue);
+
+window.onclick = (e) => {
+    if(e.path[2].className == 'faq'){
+        e.stopPropagation();
+        console.log(e);
+        console.log(e.path[1].nextElementSibling);
+        let ele = e.path[1].nextElementSibling;
+        console.log(ele.style.display == "none");
+        if(ele.style.display == 'none'){
+            console.log(e.path[1].children[1]);
+            e.path[1].children[1].style = 'transform:rotate(180deg)';
+            ele.style.display = 'flex';
+        }else{
+            e.path[1].children[1].style = 'transform:rotate(0deg)';
+            ele.style.display = 'none'
+        }
+    }
+}
