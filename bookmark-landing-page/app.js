56fcd4c973d0a399117b7755da607cd13548fff6
AKHIL POKLE <akhilpokle96@gmail.com>
2020-06-03 19:34:08 +0530

    more css

--- bookmark-landing-page/app.js
+++ bookmark-landing-page/app.js
@@ -4,38 +4,45 @@ const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
 
-one.style.background='#00ff00';
-two.style.background='#ff0000';
-three.style.background = '#0000ff';
-
 let ele = document.querySelectorAll('.section-3 > div');
 
 one.onclick = function(){
     for(let i=0;i<ele.length;i++){
         ele[i].style.display = 'none';
-        console.log('hidinh',i);
-        console.log(ele[i]);
     }
-    ele[0].style.display='block';
+    one.setAttribute('id','red-border');
+    two.removeAttribute("id");
+    three.removeAttribute("id");
+    ele[0].style.display='flex';
+
 }
 
 two.onclick = function(){
     for(let i=0;i<ele.length;i++){
         ele[i].style.display = 'none';
     }
-    ele[1].style.display = 'block';
+    ele[1].style.display = 'flex';
+    two.setAttribute('id','red-border');
+    one.removeAttribute("id");
+    three.removeAttribute("id");
+
 }
 
 three.onclick = function(){
     for(let i=0;i<ele.length;i++){
         ele[i].style.display = 'none';
     }
-    ele[2].style.display = 'block';
+    ele[2].style.display = 'flex';
+    three.setAttribute('id','red-border');
+    one.removeAttribute("id");
+    two.removeAttribute("id");
+
 }
 
 window.onload = () => {
-    ele[0].style.display = 'block';
+    ele[0].style.display = 'flex';
     ele[1].style.display = 'none';
     ele[2].style.display = 'none';
+    one.setAttribute('id','red-border');
 }
 
