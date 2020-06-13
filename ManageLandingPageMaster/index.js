666b8187b28303ea71b217657bddc9677996a031
AKHIL POKLE <akhilpokle96@gmail.com>
2020-06-13 16:05:53 +0530

    file changes

---
+++ ManageLandingPageMaster/index.js
@@ -0,0 +1,25 @@
+let tags = document.querySelectorAll('nav  a');
+let divs = document.querySelectorAll('body>div');
+for(let t of tags){
+    t.addEventListener('click',show);
+}
+
+function show(e){
+    for(let d of divs){
+        d.style.display = "none";
+    }
+    console.log(e.target.dataset.id);
+    let ele = e.target.dataset.id;
+    console.log(document.getElementById(ele));
+    document.getElementById(ele).style.display='block';
+}
+
+window.onload = () =>{
+    console.log("ho");
+    for(let d of divs){
+        d.style.display = "none";
+    }
+    document.getElementById('main').style.display='block';
+}
+console.log(divs);
+console.log(tags);
