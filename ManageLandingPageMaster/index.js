dd107b40c781e218cf5d348dab87f4534ccfed3c
AKHIL POKLE <akhilpokle96@gmail.com>
2020-06-15 17:27:21 +0530

    slideeerrrrr

--- ManageLandingPageMaster/index.js
+++ ManageLandingPageMaster/index.js
@@ -1,5 +1,9 @@
 let tags = document.querySelectorAll('a');
 let divs = document.querySelectorAll('body>div');
+
+let monthly = document.getElementsByClassName('monthly');
+let yearly = document.getElementsByClassName('yearly');
+
 for(let t of tags){
     t.addEventListener('click',show);
 }
@@ -10,16 +14,48 @@ function show(e){
     }
     console.log(e.target.dataset.id);
     let ele = e.target.dataset.id;
-    console.log(document.getElementById(ele));
     document.getElementById(ele).style.display='block';
 }
 
 window.onload = () =>{
-    console.log("ho");
     for(let d of divs){
         d.style.display = "none";
     }
     document.getElementById('main').style.display='block';
+    console.log(monthly);
+    console.log(yearly);
+    for(let m of monthly){
+        m.style.display = 'none';
+    }
 }
-console.log(divs);
-console.log(tags);
+
+
+let prices = document.querySelectorAll('#pricing > .flex > .grid > div');
+console.log(prices[0]);
+console.log(prices[1]);
+
+let pricesContainer = document.querySelector('#pricing > .flex > .grid');
+pricesContainer.addEventListener('click',compute);
+
+function compute(){
+    if(prices[0].getAttribute('id')){
+        prices[0].removeAttribute('id');
+        prices[1].setAttribute('id','active');
+        for(let y of yearly){
+            y.style.display="none";
+        }
+        for(let m of monthly){
+            m.style.display="inline-block";
+        }
+
+    }else{
+        prices[1].removeAttribute('id');
+        prices[0].setAttribute('id','active');
+        for(let m of monthly){
+            m.style.display="none";
+        }
+        for(let y of yearly){
+            y.style.display="inline-block";
+        }        
+    }
+}
